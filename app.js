93e196c332407e4daa82aabdd964775693e46748
BritKHall <bhall5097@gmail.com>
2019-08-10 01:41:13 -0400

    Finished
    exceeds exp tasks done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
 
 // ============Fetch====================================
 
@@ -17,26 +19,27 @@ fetch(urlAPI)
 
 // ============Display data ====================================
 
+
+
 function displayEmployees(employeeData) {
     employees = employeeData;
     let employeeHTML = '';
     employees.forEach((employee, index) => {
-    let name = employee.name;
-    let email = employee.email;
-    let city = employee.location.city;
-    let picture = employee.picture;
+        let name = toTitleCase(employee.name.first);
+        let surname= toTitleCase(employee.name.last);
+        let email = employee.email;
+        let city = toTitleCase (employee.location.city);
+        let picture = employee.picture;
 
-    String.prototype.capitalize = function() {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    }
+
 
     employeeHTML += `
         <div class="card" data-index="${index}">
         <img class="avatar" src="${picture.large}" />
         <div class="text-container">
-        <h2 class="name">${name.first.capitalize()} ${name.last.capitalize()}</h2>
-        <p class="email">${email.capitalize()}</p>
-        <p class="address">${city.capitalize()}</p>
+        <h2 class="name">${name } ${surname}</h2>
+        <p class="email">${email }</p>
+        <p class="address">${city}</p>
         </div>
         </div>
         `
@@ -44,7 +47,21 @@ function displayEmployees(employeeData) {
         gridContainer.innerHTML = employeeHTML;
         }
 
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function(txt){
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+
+        
 // ============ Model ====================================
+
+
 
 function displayModal(index) {
 
@@ -52,16 +69,17 @@ function displayModal(index) {
     }, picture } = employees[index];
     let date= new Date(dob.date)
 
+
     // setting up the html
     const modalHTML = ` 
         <img class="avatar" src="${picture.large}" />
         <div class="text-container">
-        <h2 class="name">${name.first.capitalize()} ${name.last.capitalize()}</h2>
-        <p class="email">${email.capitalize()}</p>
-        <p class="address">${city.capitalize()}</p>
+        <h2 class="name">${name.first.capitalize() } ${name.last.capitalize()  }</h2>
+        <p class="email">${email }</p>
+        <p class="address">${city.capitalize()  }</p>
         <hr />
         <p>${phone}</p>
-        <p class="address">${street}, ${state.capitalize()} ${postcode}</p>
+        <p class="address">${street}, ${state } ${postcode}</p>
         <p>Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
@@ -69,7 +87,10 @@ function displayModal(index) {
 
     // unhide the modal
         overlay.classList.remove("hidden");
+        overlay.setAttribute("data-index",`${index}`)
         modalContainer.innerHTML = modalHTML;
+
+    
 }
 
 //adding the click events
@@ -87,27 +108,48 @@ modalClose.addEventListener('click', () => {
     
 //moving from one mod to next
 
+rightArrow.addEventListener("click", (e)=>{
+    let currentIndex = e.target.parentNode.parentNode.getAttribute("data-index")
+    currentIndex=parseInt(currentIndex)
+    var cards= document.querySelectorAll(".card")
+     if(currentIndex < cards.length-1){
+        var newIndex= currentIndex+1;
+        displayModal(newIndex);
+     }else if (newIndex >cards.length){
+     	newIndex=0
+     	displayModal(newIndex)}
+   
+  });
 
-
+leftArrow.addEventListener("click", (e)=>{
+    let dataIndex = e.target.parentNode.parentNode.getAttribute("data-index")
+    let currentIndex=parseInt(dataIndex)
+    var cards= document.querySelectorAll(".card")
+     if(currentIndex >=1){
+        var newIndex= currentIndex-1;
+        displayModal(newIndex);
+        console.log("new",newIndex); console.log("ddd"); console.log("current",currentIndex)
+     }
+   else if(dataIndex=="0" ){
+   	console.log("done")
+   }
+  });
 
 
 
 
 // ============ Search ====================================
 
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
+
+function hideEmployee(){
+
+    let inputField = document.getElementById('search').value.toLowerCase();
+    var cards= document.querySelectorAll(".card")
+    cards.forEach( card=> card.classList.add("hidecard"))
+    
+    cards.forEach( card=> card.innerText.indexOf(inputField)>-1 ? card.classList.remove("hidecard"):"")
+
 }
+
+const searchBar = document.getElementById("search");
+searchBar.addEventListener("keyup", hideEmployee);
