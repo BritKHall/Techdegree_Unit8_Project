4c01d4e9b8c8e48f1006bc8a507490fc2a9a6975
BritKHall <bhall5097@gmail.com>
2019-08-10 02:28:56 -0400

    Finished
    finished

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ function displayEmployees(employeeData) {
         let city = toTitleCase (employee.location.city);
         let picture = employee.picture;
 
-
-
     employeeHTML += `
         <div class="card" data-index="${index}">
         <img class="avatar" src="${picture.large}" />
@@ -76,10 +74,10 @@ function displayModal(index) {
         <div class="text-container">
         <h2 class="name">${name.first.capitalize() } ${name.last.capitalize()  }</h2>
         <p class="email">${email }</p>
-        <p class="address">${city.capitalize()  }</p>
+        <p class="address">${city.capitalize() }</p>
         <hr />
         <p>${phone}</p>
-        <p class="address">${street}, ${state } ${postcode}</p>
+        <p class="address">${street}, ${state.capitalize() } ${postcode}</p>
         <p>Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
@@ -105,35 +103,42 @@ gridContainer.addEventListener('click', e => {
 modalClose.addEventListener('click', () => {
     overlay.classList.add("hidden");
     });
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
     
 //moving from one mod to next
 
 rightArrow.addEventListener("click", (e)=>{
     let currentIndex = e.target.parentNode.parentNode.getAttribute("data-index")
     currentIndex=parseInt(currentIndex)
-    var cards= document.querySelectorAll(".card")
+    const cards= document.querySelectorAll(".card")
      if(currentIndex < cards.length-1){
-        var newIndex= currentIndex+1;
+        const newIndex= currentIndex+1;
         displayModal(newIndex);
-     }else if (newIndex >cards.length){
+     }
+     else if (newIndex >cards.length){
      	newIndex=0
      	displayModal(newIndex)}
    
-  });
+});
 
 leftArrow.addEventListener("click", (e)=>{
     let dataIndex = e.target.parentNode.parentNode.getAttribute("data-index")
     let currentIndex=parseInt(dataIndex)
-    var cards= document.querySelectorAll(".card")
+    const cards= document.querySelectorAll(".card")
      if(currentIndex >=1){
-        var newIndex= currentIndex-1;
+        const newIndex= currentIndex-1;
         displayModal(newIndex);
         console.log("new",newIndex); console.log("ddd"); console.log("current",currentIndex)
      }
    else if(dataIndex=="0" ){
    	console.log("done")
    }
-  });
+});
 
 
 
@@ -144,12 +149,12 @@ leftArrow.addEventListener("click", (e)=>{
 function hideEmployee(){
 
     let inputField = document.getElementById('search').value.toLowerCase();
-    var cards= document.querySelectorAll(".card")
-    cards.forEach( card=> card.classList.add("hidecard"))
-    
-    cards.forEach( card=> card.innerText.indexOf(inputField)>-1 ? card.classList.remove("hidecard"):"")
+    const cards= document.querySelectorAll(".card")
 
+    cards.forEach( card=> card.classList.add("hidecard"))
+    cards.forEach( card=> card.innerText.indexOf(inputField)>-1 ? card.classList.remove("hidecard"):"")
 }
+   
 
 const searchBar = document.getElementById("search");
 searchBar.addEventListener("keyup", hideEmployee);
