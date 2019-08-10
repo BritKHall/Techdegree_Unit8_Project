323543a2a84d0b2c89049c083f406c9091a966c1
BritKHall <bhall5097@gmail.com>
2019-08-09 22:32:38 -0400

    JS complete
    Basic requirements met

--- app.js
+++ app.js
@@ -17,68 +17,81 @@ fetch(urlAPI)
 
 // ============Display data ====================================
 
-    function displayEmployees(employeeData) {
-        employees = employeeData;
-        // store the employee HTML as we create it
-        let employeeHTML = '';
-        // loop through each employee and create HTML markup
-        employees.forEach((employee, index) => {
-        let name = employee.name;
-        let email = employee.email;
-        let city = employee.location.city;
-        let picture = employee.picture;
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+    employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
 
-        employeeHTML += `
-<div class="card" data-index="${index}">
-<img class="avatar" src="${picture.large}" />
-<div class="text-container">
-<h2 class="name">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${city}</p>
-</div>
-</div>
-`
-});
-gridContainer.innerHTML = employeeHTML;
-}
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+
+    employeeHTML += `
+        <div class="card" data-index="${index}">
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+        <h2 class="name">${name.first.capitalize()} ${name.last.capitalize()}</h2>
+        <p class="email">${email.capitalize()}</p>
+        <p class="address">${city.capitalize()}</p>
+        </div>
+        </div>
+        `
+        });
+        gridContainer.innerHTML = employeeHTML;
+        }
 
 // ============ Model ====================================
 
 function displayModal(index) {
-    // use object destructuring make our template literal cleaner
+
     let { name, dob, phone, email, location: { city, street, state, postcode
     }, picture } = employees[index];
-    let date = new Date(dob.date);
-    const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-<h2 class="name">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${city}</p>
-<hr />
-<p>${phone}</p>
-<p class="address">${street}, ${state} ${postcode}</p>
-<p>Birthday:
-${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-</div>
-`;
-overlay.classList.remove("hidden");
-modalContainer.innerHTML = modalHTML;
+    let date= new Date(dob.date)
+
+    // setting up the html
+    const modalHTML = ` 
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+        <h2 class="name">${name.first.capitalize()} ${name.last.capitalize()}</h2>
+        <p class="email">${email.capitalize()}</p>
+        <p class="address">${city.capitalize()}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${street}, ${state.capitalize()} ${postcode}</p>
+        <p>Birthday:
+        ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+        `;
+
+    // unhide the modal
+        overlay.classList.remove("hidden");
+        modalContainer.innerHTML = modalHTML;
 }
 
-
+//adding the click events
 gridContainer.addEventListener('click', e => {
-    // make sure the click is not on the gridContainer itself
     if (e.target !== gridContainer) {
-    // select the card element based on its proximity to actual element
-    clicked
     const card = e.target.closest(".card");
     const index = card.getAttribute('data-index');
     displayModal(index);
     }
     });
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+    });
     
-    modalClose.addEventListener('click', () => {
-        overlay.classList.add("hidden");
-        });
-        
+//moving from one mod to next
+
+
+
+
+
+
+
+// ============ Search ====================================
+
